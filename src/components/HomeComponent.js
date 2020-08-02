153d9f7d116c044606faa235a45e4075a08bb238
Chinonso Obidike <gloryshines4ever@yahoo.com>
2020-08-01 21:26:56 -0400

    conflicts resolved

--- src/components/HomeComponent.js
+++ src/components/HomeComponent.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 import React, { Component } from "react";
 import fetchVids, { baseParams } from "../axios/fetchVids";
 import SearchBar from "./SearchBar";
@@ -56,61 +55,6 @@ class Home extends Component {
           <div className="row">
             <div className="col-md-6">
               <ReactPlayer height="300px" width="" url="https://www.youtube.com/watch?v=fMUxzrgZvZQ" />
-=======
-import React, { Component } from 'react'
-import fetchVids, { baseParams } from '../axios/fetchVids'
-import SearchBar from './SearchBar'
-import VideoList from './VideoList'
-import { Profile } from './auth/Profile'
-export default class Home extends Component {
-    constructor(props) {
-        super(props)
-        this.state = {
-            videos: [],
-            selectedVideo: null
-        };
-    }
-    handleSubmit = async (searchTerm) => {
-        // fetch(`https://www.googleapis.com/youtube/v3/search?part=snippet&q=${searchTerm}&type=video&order=date&maxResults=5&key=${KEY}`)
-        //     .then(resp => resp.json())
-        //     .then((resp) => {
-        //         console.log(resp);
-        //         this.setState({ videos: resp.items });
-        //     })
-        //     .catch(error => {
-        //         console.log(error)
-        //     });
-        const response = await fetchVids.get('/search',
-            {
-                params: {
-                    ...baseParams,
-                    q: searchTerm
-                }
-            })
-            .catch(error => {
-                console.log(error)
-            });
-        this.setState({
-            videos: response.data.items
-        })
-        console.log(response.data.items)
-    }
-    handleVideoSelect = vid => {
-        this.setState({ selectedVideo: vid })
-    }
-    render() {
-        return (
-            <div className="container">
-                <div className="row">
-                    <SearchBar handleSubmit={this.handleSubmit} />
-                </div>
-                <div className="row">
-                    <div className="col-md-8">
-                        <VideoList handleVideoSelect={this.handleVideoSelect} videos={this.state.videos} />
-                    </div>
-                </div>
-                {/* <Profile /> */}
->>>>>>> temp-branch
             </div>
             <div className="box-2 col-md-6">
               <div className="col-12">
